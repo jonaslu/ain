40f883db6999e8d348207e7d20b7aa8d128e47dd
Jonas Lundberg <jonasl@iamjonas.me>
2021-12-17 20:49:29 +0100

    Add error message if pipe is empty
    If there are no file names sent over the
    pipe, fail with an error message on
    pipe empty.
    
    Test plan:
    * Issue echo '' | ain and verify
      that ain fails with an error message
      sayin' Pipe input did not contain any
      template names.

--- internal/pkg/disk/filename.go
+++ internal/pkg/disk/filename.go
@@ -30,7 +30,11 @@ func GetTemplateFilenames() ([]string, error) {
 
 		localTemplateFilenamesViaPipe, err := utils.TokenizeLine(string(fileNameBytes), true)
 		if err != nil {
-			return nil, errors.Wrap(err, "Could not parse filenames from pipe")
+			return nil, errors.Wrap(err, "Could not read template name(s) from pipe")
+		}
+
+		if len(localTemplateFilenamesViaPipe) == 0 {
+			return nil, errors.New("Pipe input did not contain any template names")
 		}
 
 		localTemplateFilenames = append(localTemplateFilenames, localTemplateFilenamesViaPipe...)
