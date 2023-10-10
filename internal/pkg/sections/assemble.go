234ce72e7525f7f1b9fbc998289ea0d84fff3515
Jonas Lundberg <jonasl@iamjonas.me>
2023-10-10 20:44:35 +0200

    Assemble and validate [Backend]
    Mostly c'n'p:ed from the previous backend
    capturing. Checks for several lines
    under backend (no cruft in there),
    if backend is known and is misspelled.
    Overwrites across templates.
    
    Test plan:
    * Have a [Backend] section with
      several lines, where the first
      line is misspelled and the second is
      a valid backend. Verify this produces
      an error on several lines, regardless of
      misspelling or valid backends.
    * Have a [Backend] section with an unknown backend
      such as karate. Verify produces an error
      on unknown backend.
    * Have a [Backend] section with a misspelled
      backend such as cvrl. Verify error message
      gives a levensthein corrected error message
      (did you mean curl?).
    * Have two files with [Backend] sections.
      With wget in the first and curl in the
      second. Verify curl is selected and wget
      is overridden.
    * Have a template with no [Backend] section.
      Verify results in an error-message on
      No mandatory [Backend] found.

--- internal/pkg/sections/assemble.go
+++ internal/pkg/sections/assemble.go
@@ -6,8 +6,10 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/jonaslu/ain/internal/pkg/call"
 	"github.com/jonaslu/ain/internal/pkg/data"
 	"github.com/jonaslu/ain/internal/pkg/disk"
+	"github.com/jonaslu/ain/internal/pkg/utils"
 )
 
 func Assemble(ctx context.Context, filenames []string) (*data.BackendInput, string, error) {
@@ -96,23 +98,62 @@ func Assemble(ctx context.Context, filenames []string) (*data.BackendInput, stri
 		return nil, strings.Join(fatals, "\n\n"), nil
 	}
 
-	var host string
+	var host, backend string
 
 	for _, sectionedTemplate := range allSectionedTemplates {
 		for _, hostSourceMarker := range *sectionedTemplate.GetNamedSection(HostSection) {
 			host = host + hostSourceMarker.LineContents
 		}
-	}
 
-	if host == "" {
-		fatals = append(fatals, "No mandatory [Host] section found")
+		backendSourceMarkers := *sectionedTemplate.GetNamedSection(BackendSection)
+		if len(backendSourceMarkers) > 1 {
+			sectionedTemplate.SetFatalMessage("Found several lines under [Backend]", backendSourceMarkers[0].SourceLineIndex)
+		} else if len(backendSourceMarkers) == 1 {
+			backendSourceMarker := backendSourceMarkers[0]
+			requestedBackendName := strings.ToLower(backendSourceMarker.LineContents)
+
+			if !call.ValidBackend(requestedBackendName) {
+				foundMisspelledName := false
+				for backendName, _ := range call.ValidBackends {
+					if utils.LevenshteinDistance(requestedBackendName, backendName) < 3 {
+						sectionedTemplate.SetFatalMessage(fmt.Sprintf("Unknown backend: %s. Did you mean %s", requestedBackendName, backendName), backendSourceMarker.SourceLineIndex)
+						foundMisspelledName = true
+					}
+				}
+
+				if !foundMisspelledName {
+					sectionedTemplate.SetFatalMessage(fmt.Sprintf("Unknown backend %s", requestedBackendName), backendSourceMarker.SourceLineIndex)
+				}
+			}
+
+			backend = requestedBackendName
+		}
+
+		if sectionedTemplate.HasFatalMessages() {
+			fatals = append(fatals, sectionedTemplate.GetFatalMessages())
+		}
 	}
 
 	if len(fatals) > 0 {
 		return nil, strings.Join(fatals, "\n\n"), nil
 	}
 
-	fmt.Println(host)
+	if host == "" {
+		fatals = append(fatals, "No mandatory [Host] section found")
+	}
+
+	if backend == "" {
+		fatals = append(fatals, "No mandatory [Backend] section found")
+	}
+
+	if len(fatals) > 0 {
+		// Since we no longer have a sectionedTemplate errors
+		// are no longer linked to a file and we separate
+		// with one newline
+		return nil, strings.Join(fatals, "\n"), nil
+	}
+
+	fmt.Println(host, backend)
 
 	return nil, "", nil
 }
