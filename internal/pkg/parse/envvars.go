903600934525fb8163756fdc57e813bb9645b41e
Jonas Lundberg <jonasl@iamjonas.me>
2024-03-04 08:57:12 +0100

    Bug: two or more env-vars on the same line
    Ain incorrectly made a line with two env-vars
    into two separate lines.
    
    Change strategy: aggregate all env-vars
    and their values on a line first into
    a temp-struct.
    
    Then iterate over that temp-struct and
    splice in the values into the original
    line.
    
    This handles the case where a ${VAR}
    contains the actual value ${VAR} - so
    we're not infinitely looping on the same
    replacement.
    
    Test plan:
    * Have a template with an one
      line ${VAR}. Make sure VAR is
      undefined. Verify error on the ${VAR}
      line.
    * Have a template with one line
      and ${VAR1} ${VAR2}. Set both to
      undefined. Verify error on
      both variables.
    * Set ${VAR1} to some value and
      verify error on the second variable.
    * Have a template with no variables
      and verify lines are passed through
      (i e no regression).
    * Have a template with ${VAR1}.
      Set VAR1='${VAR1}'$'\n''$(ff)'
      and run the template. Verify via
      the expanded context that the '${VAR1}'
      is not expanded again (and again).
    * Have a template with ${VAR1} ${VAR2}.
      Set VAR1="[Host]\nlocalhost\n[Backend]\ncurl\n"
      VAR2="[Headers]\nContent-Type: ugh\n"
      in the .env file.
      Verify that the resulting ain -p
      call handles newlines in variables still.

--- internal/pkg/parse/envvars.go
+++ internal/pkg/parse/envvars.go
@@ -55,6 +55,13 @@ func (s *sectionedTemplate) substituteEnvVars() {
 		lineContents := expandedTemplateLine.LineContents
 		noCommentsLineContents, _, _ := strings.Cut(expandedTemplateLine.LineContents, "#")
 
+		foundEnvVarValues := []struct {
+			value              string
+			envVarWithBrackets string
+		}{}
+
+		prevNumFatals := len(s.fatals)
+
 		for _, envVarWithBrackets := range envVarExpressionRe.FindAllString(noCommentsLineContents, -1) {
 			envVarKeyStr := envVarKeyRe.FindStringSubmatch(envVarWithBrackets)
 			if len(envVarKeyStr) != 2 {
@@ -83,25 +90,50 @@ func (s *sectionedTemplate) substituteEnvVars() {
 				continue
 			}
 
-			expandedLines := strings.Replace(lineContents, envVarWithBrackets, value, 1)
-			splitExpandedLines := strings.Split(strings.ReplaceAll(expandedLines, "\r\n", "\n"), "\n")
-
-			for _, splitExpandedLine := range splitExpandedLines {
-				localExpandedTemplateLines = append(localExpandedTemplateLines, expandedSourceMarker{
-					sourceMarker: sourceMarker{
-						LineContents:    splitExpandedLine,
-						SourceLineIndex: expandedTemplateLine.SourceLineIndex,
-					},
-					expanded: true,
-				})
-			}
+			foundEnvVarValues = append(foundEnvVarValues, struct {
+				value              string
+				envVarWithBrackets string
+			}{value, envVarWithBrackets})
 		}
 
-		if len(localExpandedTemplateLines) == 0 {
+		if len(foundEnvVarValues) == 0 {
 			newExpandedTemplateLines = append(newExpandedTemplateLines, expandedTemplateLine)
-		} else {
-			newExpandedTemplateLines = append(newExpandedTemplateLines, localExpandedTemplateLines...)
+			continue
 		}
+
+		if prevNumFatals < len(s.fatals) {
+			// When the fatal is set it reads from the expanded source
+			// lines as it happens. The stuff below will never be read.
+			continue
+		}
+
+		var expandedResult string
+		tempLineContents := lineContents
+
+		for _, foundValue := range foundEnvVarValues {
+			foundIndex := strings.Index(tempLineContents, foundValue.envVarWithBrackets)
+
+			expandedResult += tempLineContents[:foundIndex]
+			expandedResult += foundValue.value
+
+			tempLineContents = tempLineContents[foundIndex+len(foundValue.envVarWithBrackets):]
+		}
+
+		expandedResult += tempLineContents
+
+		splitExpandedLines := strings.Split(strings.ReplaceAll(expandedResult, "\r\n", "\n"), "\n")
+
+		for _, splitExpandedLine := range splitExpandedLines {
+			localExpandedTemplateLines = append(localExpandedTemplateLines, expandedSourceMarker{
+				sourceMarker: sourceMarker{
+					LineContents:    splitExpandedLine,
+					SourceLineIndex: expandedTemplateLine.SourceLineIndex,
+				},
+				expanded: true,
+			})
+		}
+
+		newExpandedTemplateLines = append(newExpandedTemplateLines, localExpandedTemplateLines...)
 	}
 
 	s.expandedTemplateLines = newExpandedTemplateLines
