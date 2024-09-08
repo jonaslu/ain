99b367668642d3955173859db30bb41428caf0d0
Jonas Lundberg <jonasl@iamjonas.me>
2024-09-08 21:48:16 +0200

    Add first stab at e2e tests
    Regression is a thing.
    
    Setup is a runnable
    template with yaml as
    comments last in the file.
    
    Yaml because it's a
    fairly readable format and
    commented out because then
    the file is self-containing
    and can act as examples.
    
    Currently passes in:
    env-vars, args
    and then compares the
    stdout, stderr and exitcode.
    
    Test harness builds a binary
    then runs the tests.
    All hooked into go test.
    
    Test plan:
    * go test ./... includes
      e2e tests, which passes.

---
+++ e2e/e2e_test.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	"testing"
+	"time"
+
+	"gopkg.in/yaml.v3"
+)
+
+const testBinaryPath = "./ain_test"
+
+type testDirectives struct {
+	Env      []string
+	Args     []string
+	Stderr   string
+	Stdout   string
+	ExitCode int
+}
+
+func buildGoBinary() error {
+	// cmd := exec.Command("go", "build", "-cover", "-o", testBinaryPath, "../cmd/ain/main.go")
+	cmd := exec.Command("go", "build", "-o", testBinaryPath, "../cmd/ain/main.go")
+	err := cmd.Run()
+	if err != nil {
+		return errors.New("could not build binary")
+	}
+
+	return nil
+}
+
+func runTest(filename string, templateContents []byte) error {
+	lines := strings.Split(string(templateContents), "\n")
+	idx := len(lines)
+
+	directives := []string{}
+
+	for idx >= 0 {
+		idx--
+
+		line := string(lines[idx])
+		if line == "" && len(directives) == 0 {
+			continue
+		}
+
+		if !strings.HasPrefix(line, "# ") {
+			break
+		}
+
+		trimmedPrefixLine := strings.TrimPrefix(line, "# ")
+
+		directives = append([]string{trimmedPrefixLine}, directives...)
+	}
+
+	testDirectives := testDirectives{}
+	err := yaml.Unmarshal([]byte(strings.Join(directives, "\n")), &testDirectives)
+
+	if err != nil {
+		return errors.New("Could not unmarshal yaml")
+	}
+
+	var stdout, stderr bytes.Buffer
+
+	// !! TODO !! Get timeout from yaml or default to 1 seconds
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	cmd := exec.CommandContext(ctx, "./ain_test", append(testDirectives.Args, filename)...)
+	cmd.Env = testDirectives.Env
+	cmd.Env = append(cmd.Env, "PATH="+os.Getenv("PATH"))
+	// cmd.Env = append(cmd.Env, "GOCOVERDIR=templates/gcov")
+
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+
+	err = cmd.Start()
+	if err != nil {
+		return errors.New("could not start command")
+	}
+
+	err = cmd.Wait()
+	if err != nil && err.(*exec.ExitError) == nil {
+		return errors.New("Could not wait for command")
+	}
+
+	if ctx.Err() == context.DeadlineExceeded {
+		return errors.New("timed out")
+	}
+
+	if ctx.Err() != nil {
+		return errors.New("context error")
+	}
+
+	if stderr.String() != testDirectives.Stderr {
+		return fmt.Errorf("stderr %s did not match %s", stderr.String(), testDirectives.Stderr)
+	}
+
+	if stdout.String() != testDirectives.Stdout {
+		return fmt.Errorf("stdout %s did not match %s", stdout.String(), testDirectives.Stdout)
+	}
+
+	exitCode := cmd.ProcessState.ExitCode()
+	if exitCode != testDirectives.ExitCode {
+		return errors.New("exit code did not match")
+	}
+
+	return nil
+}
+
+func readTestFiles() (map[string][]byte, error) {
+	templateFolder := "templates"
+	files, err := os.ReadDir(templateFolder)
+
+	if err != nil {
+		return nil, errors.New("could not read directory")
+	}
+
+	testFiles := map[string][]byte{}
+
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		fileName := templateFolder + "/" + file.Name()
+		if !strings.HasSuffix(fileName, ".ain") {
+			continue
+		}
+
+		res, err := os.ReadFile(fileName)
+		if err != nil {
+			return nil, errors.New("could not read file")
+		}
+
+		testFiles[fileName] = res
+	}
+
+	return testFiles, nil
+}
+
+func Test_main(t *testing.T) {
+	var coverage bool
+
+	flag.BoolVar(&coverage, "coverage", false, "Enable coverage")
+	flag.Parse()
+
+	if err := buildGoBinary(); err != nil {
+		t.Fatalf("Could not build binary")
+		return
+	}
+
+	defer os.Remove(testBinaryPath)
+
+	files, err := readTestFiles()
+	if err != nil {
+		t.Fatalf("Could not read test templates")
+		return
+	}
+
+	for filename, testContents := range files {
+		t.Run(filename, func(t *testing.T) {
+			err := runTest(filename, testContents)
+			if err != nil {
+				t.Errorf("%s: %v", filename, err)
+			}
+		})
+	}
+}
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.13
 require (
 	github.com/hashicorp/go-envparse v0.1.0
 	github.com/pkg/errors v0.9.1
+	gopkg.in/yaml.v3 v3.0.1
 )
