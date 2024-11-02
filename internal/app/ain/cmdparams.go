5f8e2f836e2cdae2c856e9ee6a613f9c29f862b8
Jonas Lundberg <jonasl@iamjonas.me>
2024-11-02 22:20:02 +0100

    Ditch flag and args thyself
    We got into trouble with --args,
    as template names can be given
    through a pipe.
    
    The go flag package can be bent
    to handle this, but it gets ugly.
    
    Do the flag parsing ourselves.
    
    Add a redefined guard if the
    same flag is passed several times.
    
    Test plan:
    * Pass any flag other than -h and such
      as go run cmd/ain/main.go -p -p.
      Verify error message on flag passed
      twice.
    * Verify -h prints help.
    * Verify -v prints version.
    * Verify -e and no path prints
      requires an argument.
    * Verify -e .env picks up the
      environment file and passes it correctly.
    * Verify -b prints the
      basic template (but -b file is currently broken).
    * Verify go run cmd/ain/main.go--vars
      VAR=1 no longer makes ain fail on
      an undefined flag, but fails on
      missing template names instead.

--- internal/app/ain/cmdparams.go
+++ internal/app/ain/cmdparams.go
@@ -1,43 +1,159 @@
 package ain
 
 import (
-	"flag"
 	"fmt"
 	"os"
 	"strings"
 )
 
-func NewCmdParams() *CmdParams {
-	var leaveTmpFile, printCommand, showVersion, generateEmptyTemplate bool
-	var envFile string
+func printUsage(appName string, flags []flag) {
+	w := os.Stderr
 
-	flag.Usage = func() {
-		w := flag.CommandLine.Output()
-
-		introMsg := `Ain is an HTTP API client. It reads template files to make the HTTP call.
+	introMsg := `Ain is an HTTP API client. It reads template files to make the HTTP call.
 These can be given on the command line or sent over a pipe.
 
 Project home page: https://github.com/jonaslu/ain`
 
-		fmt.Fprintf(w, "%s\n\nusage: %s [OPTIONS] <template.ain> [--vars VAR=VALUE ...] \n", introMsg, os.Args[0])
-		fmt.Fprintf(w, "\nOPTIONS:\n")
-		flag.VisitAll(func(f *flag.Flag) {
-			fmt.Fprintf(w, "  -%-22s %s\n", f.Name, f.Usage)
-		})
-
-		fmt.Fprintf(w, "\nARGUMENTS:\n")
-		fmt.Fprintf(w, "  <template.ain>[!]       One or more template files to process. Required\n")
-		fmt.Fprintf(w, "  --vars VAR=VALUE [...]  Values for environment variables, set after <template.ain> file(s)\n")
+	fmt.Fprintf(w, "%s\n\nusage: %s [OPTIONS] <template.ain> [--vars VAR=VALUE ...] \n", introMsg, appName)
+	fmt.Fprintf(w, "\nOPTIONS:\n")
+	for _, f := range flags {
+		fmt.Fprintf(w, "  %-22s %s\n", f.flagName, f.usage)
 	}
 
-	flag.BoolVar(&leaveTmpFile, "l", false, "Leave any body-files")
-	flag.BoolVar(&printCommand, "p", false, "Print command to the terminal instead of executing")
-	flag.StringVar(&envFile, "e", ".env", "Path to .env file")
-	flag.BoolVar(&showVersion, "v", false, "Show version and exit")
-	flag.BoolVar(&generateEmptyTemplate, "b", false, "Generate basic template files(s)")
-	flag.Parse()
+	fmt.Fprintf(w, "\nARGUMENTS:\n")
+	fmt.Fprintf(w, "  <template.ain>[!]       One or more template files to process. Required\n")
+	fmt.Fprintf(w, "  --vars VAR=VALUE [...]  Values for environment variables, set after <template.ain> file(s)\n")
+}
+
+type flagConsumer func([]string) (found bool, restArgs []string, error error)
+
+type flag struct {
+	flagName     string
+	usage        string
+	flagConsumer flagConsumer
+}
+
+func makeBoolConsumer(flagName string, val *bool) flagConsumer {
+	return func(args []string) (bool, []string, error) {
+		if args[0] == flagName {
+			*val = true
+			return true, args[1:], nil
+		}
+
+		return false, args, nil
+	}
+}
+
+func makeStringConsumer(flagName string, val *string) flagConsumer {
+	return func(args []string) (bool, []string, error) {
+		if args[0] == flagName {
+			if len(args) < 2 {
+				return false, args, fmt.Errorf("flag %s requires an argument", flagName)
+			}
+
+			*val = args[1]
+
+			return true, args[2:], nil
+		}
+
+		return false, args, nil
+	}
+}
+
+func makeRedefinedGuardConsumer(name string, flagConsumer flagConsumer) flagConsumer {
+	consumed := false
+	return func(args []string) (bool, []string, error) {
+		consumerConsumed, restArgs, err := flagConsumer(args)
+
+		if consumerConsumed && consumed {
+			return false, restArgs, fmt.Errorf("flag %s passed twice", name)
+		}
+
+		consumed = consumerConsumed
+
+		return consumed, restArgs, err
+	}
+}
+
+func makeFlag(flagName, usage string, flagConsumer flagConsumer) flag {
+	return flag{
+		flagName:     flagName,
+		usage:        usage,
+		flagConsumer: flagConsumer,
+	}
+}
+
+func makeBoolFlag(flagName, usage string, val *bool) flag {
+	return makeFlag(flagName, usage, makeRedefinedGuardConsumer(flagName, makeBoolConsumer(flagName, val)))
+}
+
+func makeStringFlag(flagName, usage string, val *string) flag {
+	return makeFlag(flagName, usage, makeRedefinedGuardConsumer(flagName, makeStringConsumer(flagName, val)))
+}
+
+func NewCmdParams() *CmdParams {
+	var leaveTmpFile, printCommand, showVersion, generateEmptyTemplate, showHelp bool
+	envFile := ".env"
+
+	flags := []flag{}
+
+	appName := os.Args[0]
+	restArgs := os.Args[1:]
+
+	flags = append(flags, makeBoolFlag("-p", "Print command to the terminal instead of executing", &printCommand))
+	flags = append(flags, makeStringFlag("-e", "Path to .env file", &envFile))
+	flags = append(flags, makeBoolFlag("-l", "Leave any body-files", &leaveTmpFile))
+	flags = append(flags, makeBoolFlag("-b", "Generate basic template files(s)", &generateEmptyTemplate))
+	flags = append(flags, makeBoolFlag("-v", "Show version and exit", &showVersion))
+	flags = append(flags, makeBoolFlag("-h", "Show help and exit", &showHelp))
+
+	for {
+		if len(restArgs) == 0 {
+			break
+		}
+
+		arg := restArgs[0]
+
+		if arg == "--vars" {
+			break
+		}
+
+		if strings.HasPrefix(arg, "-") {
+			flagFound := false
+
+			for _, flag := range flags {
+				consumed, consumedRestArgs, err := flag.flagConsumer(restArgs)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "%s: %s\n", appName, err)
+					os.Exit(1)
+				}
+
+				restArgs = consumedRestArgs
+				if consumed {
+					flagFound = true
+					break
+				}
+			}
+
+			if !flagFound {
+				fmt.Fprintf(os.Stderr, "%s: unknown flag: %s\n", appName, arg)
+				os.Exit(1)
+			}
+
+			continue
+		}
+
+		// No more flags
+		break
+	}
+
+	if showHelp {
+		printUsage(appName, flags)
+		os.Exit(0)
+	}
 
 	return &CmdParams{
+		restArgs:              restArgs,
 		LeaveTmpFile:          leaveTmpFile,
 		PrintCommand:          printCommand,
 		ShowVersion:           showVersion,
@@ -50,7 +166,7 @@ func (c *CmdParams) SetEnvVarsAndFilenames() error {
 	collectVars := false
 	vars := []string{}
 
-	for _, arg := range flag.Args() {
+	for _, arg := range c.restArgs {
 		if arg == "--vars" {
 			collectVars = true
 			continue
@@ -79,6 +195,8 @@ func (c *CmdParams) SetEnvVarsAndFilenames() error {
 }
 
 type CmdParams struct {
+	restArgs []string
+
 	LeaveTmpFile          bool
 	PrintCommand          bool
 	ShowVersion           bool
